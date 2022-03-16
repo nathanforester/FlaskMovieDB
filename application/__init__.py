e5d8b50d4cd1afe47944ef24a38bcfe07f3bf250
nathanforester <nathanforester32@gmail.com>
2022-03-16 13:54:29 +0000

    added fix to automate database

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\workspaceBackup\\DevOps\\BasicProjectExample\\FlaskMovieDB\\flask.sqlite3'
+url = 'mysql+pymysql://nathan:password@final-db.cbr9zealguel.eu-west-2.rds.amazonaws.com/movies_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = url
 app.config['SECRET_KEY'] = '123456789'
 
 db = SQLAlchemy(app)
 
-import application.routes 
+engine= create_engine(url, echo=True)
+if not database_exists(engine.url):
+    create_database(engine.url)
+else:
+    engine.connect()
 
+import application.routes 

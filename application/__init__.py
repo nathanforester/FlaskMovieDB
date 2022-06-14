9cfab412fe112f9ed9da1e0aff4ab2cd91ea7d4e
Nathan Forester <Nathan.Forester@estio.co.uk>
2022-06-14 20:02:54 +0100

    added automation

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
@@ -6,7 +7,7 @@ from sqlalchemy_utils import database_exists, create_database
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-url = 'mysql+pymysql://nathan:password@final-db.cbr9zealguel.eu-west-2.rds.amazonaws.com/movies_db'
+url = 'mysql+pymysql://' + os.environ['USERNAME'] + ':' + os.envrion['PASSWORD'] + '@' + os.environ['DB_ENDPOINT'] + '/' + os.environ['DB_NAME']
 app.config['SQLALCHEMY_DATABASE_URI'] = url
 app.config['SECRET_KEY'] = '123456789'
 
